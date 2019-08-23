bac8b4a357f38546e9b89f062888f02a47fe7177
Daniel Oliveira <daniel.oliveira079@gmail.com>
2019-08-23 13:04:42 +0200

    Implement inmemory storage

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/danieloliveira079/php-composer-scanner
+
+go 1.12
+
+require github.com/stretchr/testify v1.4.0
---
+++ pkg/storage/inmemorystorage_test.go
@@ -0,0 +1,49 @@
+package storage
+
+import (
+	"github.com/danieloliveira079/php-composer-scanner/pkg/vulnerability"
+	"github.com/stretchr/testify/require"
+	"reflect"
+	"testing"
+)
+
+func TestInMemoryStorage_Add(t *testing.T) {
+	dummyVulnerability := vulnerability.Vulnerability{}
+
+	storage := NewInMemoryStorage()
+	storage.Add(dummyVulnerability)
+	stored := storage.GetAll()
+
+	require.NotEmpty(t, stored)
+	require.Equal(t, true, reflect.DeepEqual(dummyVulnerability, stored[0]))
+}
+
+func TestInMemoryStorage_GetAll(t *testing.T) {
+	dummyVulnerabilities := []vulnerability.Vulnerability{
+		{
+			Level:       "M",
+			Type:        "SQL Injection",
+			Versions:    []string{"5.20.12"},
+			PackageName: "adodb/adodb-php",
+			Published:   "30 Apr, 2018",
+		},
+		{
+			Level:       "M",
+			Type:        "Cross-site Scripting (XSS)",
+			Versions:    []string{"2.20.0"},
+			PackageName: "aheinze/cockpit",
+			Published:   "21 Oct, 2018",
+		},
+	}
+
+	storage := NewInMemoryStorage()
+
+	for _, v := range dummyVulnerabilities {
+		storage.Add(v)
+	}
+
+	stored := storage.GetAll()
+
+	require.NotEmpty(t, stored)
+	require.Equal(t, len(stored), len(dummyVulnerabilities))
+}
